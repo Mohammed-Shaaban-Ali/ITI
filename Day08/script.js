10298a5e7f362c28df11e55cb59894c3e7dc4a5a
Mohammed-Shaaban-Ali <ms7500746@gmail.com>
2024-07-15 12:21:26 +0300

    day 9

--- Day08/script.js
+++ Day08/script.js
@@ -1,38 +1,68 @@
-// document.getElementById("successBtn").addEventListener("click", function () {
+/**
+ * Events
+ * --------------------
+ * 
+ * == attribute on the element   => <button onclick="alert('Button clicked!')">Click me</button>
+ * 
+ * == attach function            => const button = document.getElementById('myButton');
+                                    button.onclick = function() {
+                                        alert('Button clicked!');
+                                    };
+
+ * == addEventListener            => const button = document.getElementById('myButton');
+                                    button.addEventListener('click', function() {
+                                        alert('Button clicked!');
+                                    }; 
+
+ * ---------------------------------------------------------------------------------------------
+
+ * == BOM => Browser Object Model
+
+ * --------------------
+
+ *  -- window                     => window.alert('Hello, World!');
+
+ *  -- screen                     => console.log(screen.width);
+*                                 => console.log(screen.height);
+
+ *  -- history                     => history.back();
+*                                  => history.forward();
+
+ * -- navigation                   => console.log(navigator.userAgent);
+
+ * -- location                     =>console.log(location.href); 
+*                                  =>location.href = 'https://www.example.com'; 
+
+*/
+
+// document.getElementById("errorBtn").addEventListener("click", function () {
 //   const messageDiv = document.getElementById("message");
-//   messageDiv.className = "success";
-//   messageDiv.textContent = "This is a Success Message";
-//   messageDiv.style.display = "block";
+//   messageDiv.className = "error";
+//   messageDiv.textContent = "This is an Error Message";
 // });
 
-document.getElementById("errorBtn").addEventListener("click", function () {
-  const messageDiv = document.getElementById("message");
-  messageDiv.className = "error";
-  messageDiv.textContent = "This is an Error Message";
-});
-
-function errMessage() {
-    const messageDiv = document.getElementById("message");
-  messageDiv.className = "error";
-  messageDiv.textContent = "This is an Error Message";
-   
-  }
-function success() {
-    const messageDiv = document.getElementById("message");
-    console.log(messageDiv);
-      messageDiv.className = "success";
-      messageDiv.innerHTML = "This is a Success Message";
-      messageDiv.style.display = "block";
-   
-  }
+// function errMessage() {
+//   const messageDiv = document.getElementById("message");
+//   messageDiv.className = "error";
+//   messageDiv.textContent = "This is an Error Message";
+// }
+// function success() {
+//   const messageDiv = document.getElementById("message");
+//   messageDiv.className = "success";
+//   messageDiv.innerHTML = "This is a Success Message";
+// }
+// ----------------------------------------------
 
 function changeLight() {
+
   const redLight = document.getElementById("red");
   const yellowLight = document.getElementById("yellow");
   const greenLight = document.getElementById("green");
 
   setTimeout(() => {
-    redLight.classList.add("active");
+    redLight.className +=" active"
+    greenLight.classList.remove("active");
+
   }, 1000);
 
   setTimeout(() => {
@@ -46,8 +76,11 @@ function changeLight() {
   }, 3000);
 }
 
-window.onload = changeLight;
-
+setInterval(() => {
+  changeLight()
+}, 4000);
+changeLight()
+// ----------------------------------------------
 var imageCount = 0;
 const images = [
   "images/1.jpg",
@@ -55,7 +88,8 @@ const images = [
   "images/3.jpeg",
   "images/4.webp",
 ];
-var timeout;
+
+var interval;
 
 function next() {
   imageCount == images.length - 1 ? (imageCount = 0) : imageCount++;
@@ -68,13 +102,27 @@ function prv() {
 }
 
 function play() {
-  function playImages() {
-    next();
-    timeout = setTimeout(playImages, 1000);
-  }
-  playImages();
+  interval=setInterval(function() {
+    next()
+  },1000);
 }
 
+
 function stop() {
-  clearTimeout(timeout);
+  clearInterval(interval)
 }
+// function play() {
+//   // function playImages() {
+//   // next();
+//   // timeout = setTimeout(playImages, 1000);
+//   // }
+//   // playImages();
+
+//   timeout = setInterval(function () {
+//     next()
+//   }, 1000);
+// }
+// // window.onload=play
+// function stop() {
+//   clearTimeout(timeout);
+// }
---
+++ Day09/script.js
@@ -0,0 +1,99 @@
+
+function getAllUsers() {
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", "https://jsonplaceholder.typicode.com/users");
+  xhr.send();
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState == 4) {
+      if (xhr.status == 200) {
+        var response = xhr.response;
+        var users = JSON.parse(response);
+        for (let i = 0; i < users.length; i++) {
+          displayData(users[i]);
+        }
+      }
+    }
+  };
+
+  function displayData(data) {
+    var tableBody = document.querySelector("#dataTable");
+
+    var newRow = document.createElement("tr");
+
+    var cellId = document.createElement("td");
+    cellId.innerText = data.id;
+
+    var cellName = document.createElement("td");
+    cellName.innerText = data.name;
+
+    var cellEmail = document.createElement("td");
+    cellEmail.innerText = data.email;
+
+    var cellPhone = document.createElement("td");
+    cellPhone.innerText = data.phone;
+
+    var cellWebsite = document.createElement("td");
+    cellWebsite.innerText = data.website;
+
+    newRow.appendChild(cellId);
+    newRow.appendChild(cellName);
+    newRow.appendChild(cellEmail);
+    newRow.appendChild(cellPhone);
+    newRow.appendChild(cellWebsite);
+
+    tableBody.appendChild(newRow);
+  }
+}
+
+// ---------------------------
+console.log(" --------------Task 2---------------------");
+
+function twoParameters(param1, param2) {
+  try {
+    if (arguments.length !== 2) {
+      throw "Function accepted 2 parameters only";
+    }
+    console.log(param1, param2);
+
+  } catch (error) {
+    console.log(error);
+  }
+}
+twoParameters(1, 2);
+twoParameters(1);
+twoParameters(1, 2, 3);
+
+// -----------------------------------
+console.log(" --------------Task 3---------------------");
+function sumNumbers() {
+  try {
+    var sum = 0;
+    for (let index = 0; index < arguments.length; index++) {
+      if (typeof arguments[index] !== "number") {
+        throw "Function accepted numbers only";
+      }
+      sum += arguments[index];
+    }
+    console.log("sum = " + sum);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+sumNumbers(1, 2);
+sumNumbers(2, 3, 4, 5);
+sumNumbers(2, "test");
+
+// -----------------------------------
+console.log(" --------------Task 4---------------------");
+
+function reverseParams() {
+  var reverseResult = [];
+  for (let i = arguments.length - 1; i >= 0; i--) {
+    reverseResult.push(arguments[i]);
+  }
+  return reverseResult;
+}
+
+console.log(reverseParams(1, 2, 3));
+console.log(reverseParams("test1", "test2", "test3"));
