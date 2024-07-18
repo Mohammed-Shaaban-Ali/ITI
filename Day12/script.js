b4d909232856ff85bbcc0666dccff61495249a98
Mohammed-Shaaban-Ali <ms7500746@gmail.com>
2024-07-18 11:46:05 +0300

    day 12.1

--- Day12/script.js
+++ Day12/script.js
@@ -3,43 +3,8 @@ let b=5;
 [a,b]=[b,a]
 console.log(`
   a : ${a}
-  b:${b}
+  b : ${b}
   `);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 // ------------------------------
 console.log("");
@@ -47,16 +12,15 @@ console.log("--------------- Start Task2 -----------------");
 console.log("");
 
 function getMinMax(arr) {
-  var numbers = [...arr];
-  const max = Math.max(...numbers);
-  const min = Math.min(...numbers);
-  return { min, max };
+  const max = Math.max(...arr);
+  const min = Math.min(...arr);
+  return { max,min };
 }
 
-const { min, max } = getMinMax([3, 7, 1, 9, 4, 2, 8]);
+const res = getMinMax([3, 7, 1, 9, 4, 2, 8]);
 
-console.log(`Min value: ${min}`);
-console.log(`Max value: ${max}`);
+console.log(`Min value: ${res.min}`);
+console.log(`Max value: ${res.max}`);
 
 // ----------------------
 console.log("");
@@ -66,10 +30,11 @@ console.log("");
 const fruits = ["apple", "strawberry", "banana", "orange", "mango"];
 
 const areAllStrings = fruits.every((fruit) => typeof fruit === "string");
-console.log(`Are all elements strings? ${areAllStrings}`);
+console.log(areAllStrings);
 console.log("-------------");
+
 const someStartWithA = fruits.some((fruit) => fruit.startsWith("a"));
-console.log(`Do some elements start with 'a'? ${someStartWithA}`);
+console.log(someStartWithA);
 console.log("-------------");
 
 const filteredFruits = fruits.filter(
