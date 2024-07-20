d033b62eee7ad12ef720080a187d3318a3316a48
Mohammed-Shaaban-Ali <ms7500746@gmail.com>
2024-07-20 09:02:54 +0300

    day 12.2

--- Day12/script.js
+++ Day12/script.js
@@ -1,6 +1,7 @@
-let a=3;
-let b=5;
-[a,b]=[b,a]
+let a = 3;
+let b = 5;
+[a, b] = [b, a];
+console.log("--------------- Start Task1 -----------------");
 console.log(`
   a : ${a}
   b : ${b}
@@ -12,39 +13,38 @@ console.log("--------------- Start Task2 -----------------");
 console.log("");
 
 function getMinMax(arr) {
-  const max = Math.max(...arr);
-  const min = Math.min(...arr);
-  return { max,min };
+  let max = Math.max(...arr);
+  let min = Math.min(...arr);
+  return {max, min};
 }
 
-const res = getMinMax([3, 7, 1, 9, 4, 2, 8]);
-
-console.log(`Min value: ${res.min}`);
-console.log(`Max value: ${res.max}`);
+let {min,max} = getMinMax([2, 6, 3, 4]);
+console.log("Max: ",max);
+console.log("Min: ",min);
 
 // ----------------------
+
 console.log("");
 console.log("--------------- Start Task3 -----------------");
 console.log("");
 
 const fruits = ["apple", "strawberry", "banana", "orange", "mango"];
 
-const areAllStrings = fruits.every((fruit) => typeof fruit === "string");
-console.log(areAllStrings);
+const areStrings = fruits.every((item) => typeof item === "string");
+console.log(`Every :  ${areStrings}`);//true
 console.log("-------------");
 
-const someStartWithA = fruits.some((fruit) => fruit.startsWith("a"));
-console.log(someStartWithA);
+const someStart = fruits.some((item) => item.startsWith("a"));
+console.log(`Some :  ${someStart}`); // true
+
 console.log("-------------");
 
-const filteredFruits = fruits.filter(
-  (fruit) => fruit.startsWith("b") || fruit.startsWith("s")
+const filtered = fruits.filter(
+  (item) => item.startsWith("b") || item.startsWith("s")
 );
-console.log("Filtered fruits:", filteredFruits);
-console.log("-------------");
+console.log("Filtered :", filtered);
+console.log("-------------");//[banana,strawberry]
 
-const likedFruits = fruits.map((fruit) => `I like ${fruit}`);
-console.log("Liked fruits:", likedFruits);
-console.log("-------------");
+const liked = fruits.map((item) => `I like ${item}`);
 
-likedFruits.forEach((likedFruit) => console.log(likedFruit));
+liked.forEach((item) => console.log(item));
